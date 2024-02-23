e5485a16ba160e3478920f75a7e2eae028e39204
zaavanalways <afangga0404@gmail.com>
2024-02-23 08:37:40 +0700

    push

---
+++ latihan_tanggal_20_februari_2024/latihan1.java
@@ -0,0 +1,129 @@
+package latihan_tanggal_20_februari_2024;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class latihan1 
+{
+	public static String getNamaBarang (String kodeBarang)
+	{
+		if (kodeBarang.length() == 5)
+		{
+			if (kodeBarang.substring(0, 3).equalsIgnoreCase("A10"))
+			{
+				if (kodeBarang.substring(3).equalsIgnoreCase("11"))
+				{
+					return "Deterjen Merk A";
+				}
+				else if (kodeBarang.substring(3).equalsIgnoreCase("12"))
+				{
+					return "Deterjen Merk B";
+				}
+				else
+				{
+					return "-";
+				}
+			}
+			else if (kodeBarang.substring(0, 3).equalsIgnoreCase("B20"))
+			{
+				if (kodeBarang.substring(3).equalsIgnoreCase("11"))
+				{
+					return "Shampoo Merk A";
+				}
+				else if (kodeBarang.substring(3).equalsIgnoreCase("12"))
+				{
+					return "Shampoo Merk B";
+				}
+				else
+				{
+					return "-";
+				}
+			}
+			else if (kodeBarang.substring(0, 3).equalsIgnoreCase("C30"))
+			{
+				if (kodeBarang.substring(3).equalsIgnoreCase("11"))
+				{
+					return "Gula Merk A";
+				}
+				else if (kodeBarang.substring(3).equalsIgnoreCase("12"))
+				{
+					return "Gula Merk B";
+				}
+				else
+				{
+					return "-";
+				}
+			}
+			else if (kodeBarang.substring(0, 3).equalsIgnoreCase("D40"))
+			{
+				if (kodeBarang.substring(3).equalsIgnoreCase("11"))
+				{
+					return "Minyak Merk A";
+				}
+				else if (kodeBarang.substring(3).equalsIgnoreCase("12"))
+				{
+					return "Minyak Merk B";
+				}
+				else
+				{
+					return "-";
+				}
+			}
+			else if (kodeBarang.substring(0, 3).equalsIgnoreCase("E50"))
+			{
+				if (kodeBarang.substring(3).equalsIgnoreCase("11"))
+				{
+					return "Softener Merk A";
+				}
+				else if (kodeBarang.substring(3).equalsIgnoreCase("12"))
+				{
+					return "Softener Merk B";
+				}
+				else
+				{
+					return "-";
+				}
+			}
+		}
+		return "-";
+	}
+	
+	public static long getPotonganHarga (String kodeVoucer)
+	{
+		if (kodeVoucer.equalsIgnoreCase("Diskon10"))
+		{
+			return 10000;
+		}
+		else if (kodeVoucer.equalsIgnoreCase("Diskon20"))
+		{
+			return 20000;
+		}
+		else if (kodeVoucer.equalsIgnoreCase("Diskon30"))
+		{
+			return 30000;
+		}
+		return 0;
+	}
+	public static long getTotalHarga (long harga, int jumlah)
+	{
+		return harga * jumlah;
+	}
+	public static long getTotalBayar (long total, long potongan)
+	{
+		return total - potongan;
+	}
+	
+	public static void main(String[] args) 
+	{
+		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+		String kodeBarang[] = new String[100];
+		int jumlahBarang[] = new int[100];
+		long hargaBarang[] = new long[100];
+		String kodeVoucer[] = new String[100];
+		String namaBarang[] = new String[100];
+		String voucer;	//apakah mempunyai voucer
+		
+       
+	}
+
+}
