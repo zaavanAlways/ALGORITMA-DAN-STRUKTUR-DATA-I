dd46f44f96ed9035f6594b2995eca005b70b8258
zaavanAlways <afangga0404@gmail.com>
2024-02-27 09:33:51 +0700

    latihan_rekursif

---
+++ latihan_tanggal_27_februari_2024/rekursif2.java
@@ -0,0 +1,16 @@
+package latihan_tanggal_27_februari_2024;
+
+public class rekursif2 {
+    public static int faktorial_rekursif(int n){
+        if(n== 0){
+            return 0;
+        }else{
+            return 2 + faktorial_rekursif(n-1);
+        }
+    }
+    public static void main(String [] args){
+        int n = 0;
+        System.out.println("n = 0 maka output adalah " + faktorial_rekursif(n));
+      
+    }
+}
