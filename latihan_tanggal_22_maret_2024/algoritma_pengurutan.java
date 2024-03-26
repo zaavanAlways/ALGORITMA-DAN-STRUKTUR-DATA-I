faf68e0d7ea3455b1b2d6ccb6f9bfcb860174c05
zaavanAlways <145992666+zaavanAlways@users.noreply.github.com>
2024-03-26 09:20:59 +0700

    up

--- latihan_tanggal_22_maret_2024/algoritma_pengurutan.java
+++ latihan_tanggal_22_maret_2024/algoritma_pengurutan.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class algoritma_pengurutan<AnyType extends Comparable<? super AnyType>> {
 
     public void bubbleSort(AnyType[] array, int n) {
@@ -29,7 +31,7 @@ public class algoritma_pengurutan<AnyType extends Comparable<? super AnyType>> {
 
     // Ascending Maksimum
     public void selection_Maks(AnyType[] array, int n) {
-        for (int i = n - 1; i > 0; i--) { // descending for(int i =0 ; i > n-1 ; i--)
+        for (int i = n - 1; i > 0; i--) { // descending for(int i =0 ; i < n-1 ; i--)
             int index_maks = 0;
             // mencari elemen maks
             for (int j = 1; j < i + 1; j++) {
@@ -44,22 +46,53 @@ public class algoritma_pengurutan<AnyType extends Comparable<? super AnyType>> {
         }
     }
 
-    //Ascending Minimum
-    public void selection_Min(AnyType[] array, int n){
-        
+    // Ascending Minimum
+    public void selection_Min(AnyType[] array, int n) {
+        for (int i = 0; i < n - 1; i++) { // descending : for(int i = n-1 ; i>0 ;i--)
+            int index_min = i;
+            // mencari elemen min
+            for (int j = i + 1; j < n; j++) {
+                if (array[j].compareTo(array[index_min]) < 0) {
+                    index_min = j;
+                }
+            }
+            // proses pertukaran
+            AnyType temp = array[index_min];
+            array[index_min] = array[i];
+            array[i] = temp;
+        }
     }
 
     public static void main(String[] args) {
-        algoritma_pengurutan<Integer> asd = new algoritma_pengurutan<>();
+        algoritma_pengurutan<Integer> asd = new algoritma_pengurutan<>(); /// untuk generic paramater pada method
+        //algoritma_pengurutan<> asd = new algoritma_pengurutan<>(); // untuk variabel int paramater pada method
 
-        int[] array1 = { 34, 64, 90, 11, 25, 12, 22 };
-        asd.bubbleSort_1(array1, array1.length);
+        Integer[] array1 = { 34, 64, 90, 11, 25, 12, 22 };
         System.out.println("Data belum urut : ");
-        System.out.println("34,64,90,11,25,12,22");
-        System.out.println("Data Terurut : ");
-        for (int i : array1) {
-            System.out.print(i + " ");
-        }
+        System.out.println(Arrays.toString(array1));
 
+        asd.bubbleSort(array1, array1.length);
+        System.out.println("Data sudah terurut : ");
+        System.out.println(Arrays.toString(array1));
+        
+        // asd.selection_Min(array1, array1.length);
+        // System.out.println("Data terurut secara minimum : ");
+        // System.out.println(Arrays.toString(array1));
+
+        // asd.selection_Maks(array1, array1.length);
+        // System.out.println("Data terurut secara maksimum : ");
+        // System.out.println(Arrays.toString(array1));
+        
+        System.out.println();
+      // tambahkan pemanggilan method selection_maks dan selection_min
+
+      algoritma_pengurutan<Integer>sku = new algoritma_pengurutan<>();
+      int[] arrays2 = {2,4,19,8,1};
+      System.out.println("Data belum urut : ");
+      System.out.println(Arrays.toString(arrays2));
+
+      sku.bubbleSort_1(arrays2, arrays2.length);
+      System.out.println("Data sudah terurut : ");
+      System.out.println(Arrays.toString(arrays2));
     }
 }
