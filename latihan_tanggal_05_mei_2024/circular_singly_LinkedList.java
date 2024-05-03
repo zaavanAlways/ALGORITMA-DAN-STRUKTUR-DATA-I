629e8b4a97d376ccb1f1413a4485f3b36fa96e65
zaavanAlways <afangga0404@gmail.com>
2024-05-03 09:09:21 +0700

    up

--- latihan_tanggal_05_mei_2024/circular_singly_LinkedList.java
+++ latihan_tanggal_05_mei_2024/circular_singly_LinkedList.java
@@ -1,74 +1,149 @@
 package latihan_tanggal_05_mei_2024;
-Class Node{
 
+class Node {
+    int data;
+    Node next;
 
-public Operasi () {
-    head = null;
-    tail = null;
-} 
-public boolean isEmpty(){
-    return head == null; // return size == 0;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
-public void insertAwal(int x){
-    Node newNode = new Node(x);
-    if(isEmpty()){
-        head = newNode;
-        tail = newNode;
-        newNode.next = head;
-        size++;
-    }else{
-        Node temp = head;
-        newNode.next = temp;
-        head = temp;
-        tail.next = head;
-        //size++
+
+class Operasi {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public Operasi() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0; // return size == 0;
+    }
+
+    public void insertAwal(int x) {
+        Node newNode = new Node(x);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+            newNode.next = head;
+        } else {
+            newNode.next = head;
+            head = newNode;
+            tail.next = head;
         }
         size++;
     }
-    public void insertAkhir(int x){
+
+    public void insertAkhir(int x) {
         Node newNode = new Node(x);
-        if(isEmpty()){
-            head= newNode;
+        if (isEmpty()) {
+            head = newNode;
             tail = newNode;
             newNode.next = head;
-        }else{
+        } else {
             tail.next = newNode;
             tail = newNode;
             tail.next = head;
         }
         size++;
     }
-    public void insertPosisi(int x, int posisi){
-        Node temp;
-        Node newNode = new Node();
-        int i, count = size;
 
-        if(isEmpty() || size < posisi){
-            System.out.println("List Kosong atau posisi melebihi size");
-        }else{
-            newNode.data = x;
-            for(i = 1 ;i < posisi;i++){
-                temp = temp.next;
-            }
-            newNode.next = temp.next;
-            temp.next = newNode;
-            size++;
+    public void insertPosisi(int x, int posisi) {
+        if (posisi < 1 || posisi > size + 1) {
+            System.out.println("Posisi tidak valid");
+            return;
         }
-    }
-    public void tampilData(){
+        if (posisi == 1) {
+            insertAwal(x);
+            return;
+        }
+        Node newNode = new Node(x);
         Node temp = head;
-        if(head != null){
-            do{
-                System.out.println("%d ", temp.data);
-                temp = temp.next;
-            }while(temp != head);
+        for (int i = 1; i < posisi - 1; i++) {
+            temp = temp.next;
         }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+    }
+
+    public void tampilData() {
+        if (isEmpty()) {
+            System.out.println("List kosong");
+        }
+        Node temp = head;
+        do {
+            System.out.printf("%d ", temp.data);
+            temp = temp.next;
+        } while (temp != head);
         System.out.println();
+    }
+
+    public void deleteAwal() {
+        if (isEmpty()) {
+            System.out.println("List Kosong");
+        }
+        if (head == tail) { // List hanya memiliki satu elemen
+            head = tail = null;
+        } else {
+            head = head.next;
+            tail.next = head;
+        }
+        size--;
+    }
+
+    public void deleteAkhir() {
+        if (isEmpty()) {
+            System.out.println("List Kosong");
+        }
+        if (head == tail) { // List hanya memiliki satu elemen
+            head = tail = null;
+        } else {
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next;
+            }
+            tail = current;
+            tail.next = head;
+        }
+        size--;
+    }
+
+    // Buat method delete posisi
+    public void deletePosisi(int posisi) {
+        if (isEmpty() || posisi < 1 || posisi > size) {
+            System.err.println("posisi tidak valid");
+        }
+        if (posisi == 1) {
+            deleteAwal();
+        }
+        Node temp = head;
+        for (int i = 1; i < posisi - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        size--;
     }
 }
 
 public class circular_singly_LinkedList {
     public static void main(String[] args) {
-        
+        Operasi asd = new Operasi();
+        asd.insertAwal(10);
+        asd.insertAwal(20);
+        asd.insertAwal(30);
+        asd.insertAkhir(40);
+        asd.tampilData();
+        asd.deleteAwal();
+        asd.tampilData();
+        asd.insertPosisi(15, 2);
+        asd.tampilData();
+        asd.deletePosisi(2);
+        asd.tampilData();
     }
 }
